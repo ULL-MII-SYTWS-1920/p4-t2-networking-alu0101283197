492f2c6527083a290f6551f97e5a262e9f92b56f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-13 13:41:11 +0000

    Deploy ULL-MII-SYTWS-1920/p4-t2-networking-alu0101283197 to github.com/ULL-MII-SYTWS-1920/p4-t2-networking-alu0101283197.git:gh-pages

---
+++ precache-manifest.6b7d596dc353d64eb4a3ad5bfdbdcdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18222fb6475036cd187f",
+    "url": "/p4-t2-networking-alu0101283197/static/css/main.4c040aea.chunk.css"
+  },
+  {
+    "revision": "18222fb6475036cd187f",
+    "url": "/p4-t2-networking-alu0101283197/static/js/main.18222fb6.chunk.js"
+  },
+  {
+    "revision": "d49f461ddd43f0b563e9",
+    "url": "/p4-t2-networking-alu0101283197/static/js/1.d49f461d.chunk.js"
+  },
+  {
+    "revision": "eeade177e3ec5c98934e",
+    "url": "/p4-t2-networking-alu0101283197/static/js/runtime~main.eeade177.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/p4-t2-networking-alu0101283197/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cfe71b13bff6eb66bbda2056033b7357",
+    "url": "/p4-t2-networking-alu0101283197/index.html"
+  }
+];
